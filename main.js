98b7c9a87d57e6ed6aa3f2082404d1d349e238d2
whoamin34505 <88585012+whoamin34505@users.noreply.github.com>
2022-10-29 21:59:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,68 @@
 document.querySelector('button').onclick = myClick;
 
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
+
 function myClick() {
-    var ras = document.getElementById('setras').value;
-    if (ras=="raz") {
-        var site = 1
-    }
-    if (ras=="sum1") {
-        var site = 2
-    }
-    if (ras=="sum2") {
-        var site = 3
-    }
-    if (ras=="n1") {
-        var site = 4
-    }
-    if (ras=="n2") {
-        var site = 5
-    }
-	var sites = ["https://whoamin34505.github.io/raz/", "https://whoamin34505.github.io/sumraz/", "https://whoamin34505.github.io/sum/", "https://whoamin34505.github.io/ch/","https://whoamin34505.github.io/chsos/"];
-    window.location.href = sites [site-1];
-    console.log (sites [site-1])
+var x1 = document.getElementById('x1').value;
+var y1 = document.getElementById('y1').value;
+z1 = document.getElementById('z1').value;
+var x2 = document.getElementById('x2').value;
+var y2 = document.getElementById('y2').value;
+z2 = document.getElementById('z2').value;
+var coordsx = 0;
+var coordsy = 0;
+var length = 0;
+var koren = 0;
+var centerx = 0;
+var centery = 0;
+var centerz = 0;
+var center = 0;
+var what = 0;
+var chlen = 0
+
+if (z1 == 0 && z2 == 0) {
+    chlen = 1;
+    coordsx = x2 - x1
+    coordsy = y2 - y1
+    length = Math.abs((coordsx * coordsx)) + Math.abs((coordsy * coordsy))
+    koren = Math.sqrt (length)
+    centerx = (x1 + x2)/2;
+    centery = (y1 + y2)/2;
+    center = 'Центр: <b>' + centerx + '; ' + centery + '</b>'
+    var coordsv = 'Координаты вектора: <b>' + coordsx + '; ' + coordsy + '</b>'
+    var dlin = 'Длина вектора: <b>' + '√' + coordsx + '<sup>2</sup>' + '√' + coordsy + '<sup>2</sup>' + ' = ' + length + ' = ' + koren + '</b>'
+} else {
+    var chlen = 0;
+    console.log (z1 + '; ' + z2)
+    coordsx = x2 - x1
+    coordsy = y2 - y1;
+    what = Number(z2) - Number(z1);
+    length = Math.abs((coordsx * coordsx)) + Math.abs((coordsy * coordsy)) + Math.abs((what * what))
+    koren = Math.sqrt (length)
+    centerx = (x1 + x2)/2;
+    centery = (y1 + y2)/2;
+    centerz = (z1 + z2)/2;
+    center = 'Центр: <b>' + centerx + '; ' + centery + '; ' + centerz + '</b>'
+    var coordsv = 'Координаты вектора: <b>' + coordsx + '; ' + coordsy + '; ' + what + '</b>'
+    var dlin = 'Длина вектора: <b>' + '√' + coordsx + '<sup>2</sup>' + '+ √' + coordsy + '<sup>2</sup>' + ' + ' + '√' + what + '<sup>2</sup>' + ' = ' + length + ' = ' + koren + '</b>'
+}
+let out1 = coordsv
+let out2 = dlin
+let out3 = center
+let out4 = '<img src="1.png" width=300px style="width: 500px; border: 1px solid #000;border-radius: 10px; box-shadow:5px 5px 10px #2B1015;"></img>'
+let out5 = '<img src="2.webp" width=300px style="width: 500px; border: 1px solid #000;border-radius: 10px; box-shadow:5px 5px 10px #2B1015;"></img>'
+document.querySelector('.out1').innerHTML = 'Мудрое дерево начало думать';
+setTimeout(() => { document.querySelector('.out2').innerHTML = out4; }, 1000);
+setTimeout(() => { document.querySelector('.out3').innerHTML = 'Мудрое дерево думает...'; }, 2000);
+setTimeout(() => { document.querySelector('.out4').innerHTML = 'МУДРОЕ ДЕРЕВО ПОДУМАЛО!!!'; }, 3500);
+setTimeout(() => { document.querySelector('.out5').innerHTML = out5; }, 5000);
+setTimeout(() => { document.querySelector('.out6').innerHTML = coordsv; }, 6000);
+setTimeout(() => { document.querySelector('.out7').innerHTML = dlin; }, 7000);
+setTimeout(() => { document.querySelector('.out8').innerHTML = center; }, 8000);
 }
